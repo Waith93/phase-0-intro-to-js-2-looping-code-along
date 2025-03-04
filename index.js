6c91d2a8fcff6f98f2753a3385b1b726644e6777
Waithera <stacywaithera@DESKTOP-E0D09HN>
2025-03-04 13:23:00 +0300

    looping along

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []; // Create an empty array to store messages
+  
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+  
+    return messages; // Return the array of messages
+  }
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--; // Decrement the number
+    }
+  }
+   
